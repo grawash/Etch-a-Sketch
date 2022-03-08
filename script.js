4933c467afb2eebd0e0827a63cf313f2fc4f38bb
zurbash <duta1101@bk.ru>
2022-03-08 18:42:03 +0400

    added function for removing current container

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const container = document.querySelector(".container");
-
-const grid = 12;
+const mainContainer = document.querySelector(".centerDiv");
+let container = document.querySelector(".container");
+const select = document.querySelector("select");
+let grid = document.querySelector("select").value;
+console.log(grid);
 //creates horizontal divs inside container
 function createDiv (){
     const newDiv = document.createElement("div");
@@ -14,9 +16,25 @@ function createDiv (){
 //creates row of divs inside horizontal divs
 function createDivRow(div){
     const newDiv = document.createElement("div");
-    newDiv.setAttribute("style" , "border: 2px black solid; flex:1 1 0;");
+    newDiv.setAttribute("style" , "border: 2px black solid; flex:1 1 0; background-color: white;");
     div.appendChild(newDiv);
 }
-for(let i=0;i<grid/2;i++){
-    createDiv();
+//function for removing container div
+function removeDiv(){
+    const ldiv = document.querySelector(".centerDiv");
+    ldiv.removeChild(ldiv.firstElementChild);
 }
+for(let i=0;i<grid/2;i++){
+    createDiv(grid);
+}
+//creates new grid when selecting grid size
+select.addEventListener('click', () =>{
+    removeDiv();
+    container = document.createElement("div");
+    container.setAttribute("style", "width:800px;height: 800px;border: 2px black solid;display: flex;flex-direction: column;")
+    mainContainer.appendChild(container);
+    grid = document.querySelector("select").value;
+    for(let i=0;i<grid/2;i++){
+        createDiv();
+    }
+})
